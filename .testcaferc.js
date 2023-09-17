4f760f7de1f73981a544b54b432bd593ee292daf
Madhavv <66034411+Madhavv024@users.noreply.github.com>
2023-09-18 03:50:30 +0530

    Initial commit

---
+++ .testcaferc.js
@@ -0,0 +1,9 @@
+let os = require("os");
+
+module.exports = {
+    skipJsErrors: true,
+    hostname: os.hostname(),
+    //other settings
+    baseUrl:"https://dev.deepthought.education/login",
+    // concurrency: 2,
+};
